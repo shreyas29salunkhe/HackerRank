82d6a114c13513a0354ed0af8fcfc48a7cb7083e
Shreyas Salunkhe <shreyassps29@gmail.com>
2021-03-19 10:58:30 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,34 @@
+package com.practice;
+
+import java.util.Scanner;
+class Arrays
+{
+    public static void main(String[] args) 
+    {
+        try{
+        int n,i,j=0;
+        Scanner s = new Scanner(System.in);
+        //System.out.print("Enter number of elements in the array:");
+        n = s.nextInt();
+        int array[] = new int[n];
+        int rev[] = new int[n];
+        //System.out.println("Enter "+n+" elements ");
+        for( i=0; i < n; i++)
+        {
+            array[i] = s.nextInt();
+        }
+        //System.out.println("Reverse of an array is :");
+        for( i=n;i>0 ; i--,j++)
+        {
+            rev[j] = array[i-1];
+            System.out.print(rev[j]+" ");
+    
+        }
+        s.close();
+        }
+        catch(Exception e)
+        {
+            
+        }
+    }
+}
